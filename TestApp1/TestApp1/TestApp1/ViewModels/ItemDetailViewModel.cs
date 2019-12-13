1c4043b9584c3c82cb499e0ac2155c0f61a1907f
Jessie <michelle3208@163.com>
2019-12-13 17:39:31 +0800

    a Tabbedsample

---
+++ TestApp1/TestApp1/TestApp1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TestApp1.Models;
+
+namespace TestApp1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
